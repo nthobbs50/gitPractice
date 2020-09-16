211fdb70b420ad21cd620d99ca606f46bf11b290
Tom Hobbs <tom.hobbs@colostate.edu>
2020-09-16 14:28:10 -0600

    Add Yellowstone names code

---
+++ YellNames.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+y.L=read_csv("/Users/Tom/Documents/NSF Yellowstone LTREB 2011/A_Tom_Master_analysis_files/Data/ProductionDataFromLewis/2018_Production.csv")
+
+y.K = read_csv("/Users/Tom/Documents/NSF Yellowstone LTREB 2011/A_Tom_Master_analysis_files/Data/experimental/production/prod15.csv")
+
+names(y.L)[1:31]
+names(y.K)[1:31]
