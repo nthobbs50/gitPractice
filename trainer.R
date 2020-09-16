70443e14e9b4aea0afd14194056121dc8e80e240
Tom Hobbs <tom.hobbs@colostate.edu>
2020-09-16 13:19:24 -0600

    Added labels, gamma distribution and comment

--- trainer.R
+++ trainer.R
@@ -1,2 +1,5 @@
 y = rnorm(10000,0,1)
-hist(y)
+hist(y, ylab = "[z]", xlab = "z", freq = FALSE)
+y=rgamma(10000, 5, 10)
+hist(y, ylab = "[z]", xlab = "z", freq = FALSE)
+#a comment to provoke a commit
